d24abbdce7c8d0ac567bc793fd4bc29da2339e71
Hlofiys <hlofiys@gmail.com>
2022-08-30 11:58:29 +0300

    Initial

---
+++ src/main/java/net/hlofiys/chinaland/ChinaLandClient.java
@@ -0,0 +1,10 @@
+package net.hlofiys.chinaland;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChinaLandClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
